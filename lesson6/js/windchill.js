e12a4e056ac90c3ee14c9e18eed6bc44aef9bd6f
Use your GitHub username <Enter your email adderss>
2021-02-13 23:04:04 +0900

    initial commit

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,23 +1,28 @@
 function doInputOutput(){
-    let temp = 54;//parseFloat(document.getElementById("temp"));
+    let temp = 49;//parseFloat(document.getElementById("temp"));
     let speed = 12;//parseFloat(document.getElementById("Speed"));
     let display = windChill(temp,speed);
 
-   // if(temp>50){
-     //   if(speed<4.8)
-      //  var display = N\/A;
+  //  if(temp>50&&speed<4.8){
+      
+     //   var display = "N\/A";
+     //   document.getElementById('wind').innerHTML = display;
     //}else{
         document.getElementById('wind').innerHTML = display;
 
    
-    //}
+    
  
 }
 
 function windChill(t,s){
+    if(t>50 && s< 4.8){
+        chillFactor= "N\/A";
+        return chillFactor;
+    }else{
 
     let chillFactor = 35.74 + 0.6215 * t - 35.75*Math.pow(s,0.16) + 0.4275*t *Math.pow(s,0.16);
     return chillFactor.toFixed(2);
 }
 
-
+}
