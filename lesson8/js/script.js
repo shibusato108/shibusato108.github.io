f906acadda1f505f75d776a0b6883f0398c84b1a
Use your GitHub username <Enter your email adderss>
2021-02-27 14:01:10 +0900

    initial commit

--- lesson8/js/script.js
+++ lesson8/js/script.js
@@ -16,3 +16,16 @@ function Settoday(){
     }
      
   }
+
+
+  function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
+
+function selectResponse() {
+	const s = document.querySelector('#selected')
+	const sel = document.querySelector('#selectbrowser');
+	s.style.display = "block";
+	s.textContent = sel.value;
+	
+}
