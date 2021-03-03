f9111749d8072fdf207749652d1e31f01647beda
Use your GitHub username <Enter your email adderss>
2021-03-04 00:57:07 +0900

    initial commit

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -5,7 +5,7 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    //console.table(jsonObject);  // temporary checking for valid response and data parsing
+   // console.table(jsonObject);  // temporary checking for valid response and data parsing
     const prophets = jsonObject['prophets'];
         for (let i = 0; i < prophets.length; i++ ) {
             let card = document.createElement('section');
@@ -18,10 +18,12 @@ fetch(requestURL)
             birth.textContent = "Birth Date: " + prophets[i].birthdate;
             place.textContent = "Birth Place : " + prophets[i].birthplace;
             image.setAttribute('src', prophets[i].imageurl);
+            image.setAttribute('alt', (prophets[i].name +" " +prophets[i].lastname + "-" + prophets[i].order));
 
             card.appendChild(h2);
             card.appendChild(birth);
             card.appendChild(place);
+            card.appendChild(image);
             card.setAttribute("class", "card");
             
             document.querySelector("div.cards").appendChild(card);
@@ -31,9 +33,3 @@ fetch(requestURL)
 
 
   
-
-
-
-card.appendChild(h2);
-
-document.querySelector('div.cards').appendChild(card);
