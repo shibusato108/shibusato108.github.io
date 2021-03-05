e4f84c05cb49ba857747a4469e90001c0a83a672
Use your GitHub username <Enter your email adderss>
2021-03-06 01:48:12 +0900

    initial commit

--- lesson9/js/script.js
+++ lesson9/js/script.js
@@ -1,31 +1,36 @@
-function hamburger(){
+const requestURL ='https://byui-cit230.github.io/weather/data/towndata.json';
 
-    
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
 
-    document.getElementById("primaryNav").classList.toggle("hide");
-}
-
-function Settoday(){
-    var today = new Date() ;
-   // var weekday = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ] ;
-   var weekday = today.getDate(); 
-    if(weekday== "5"){
-        document.getElementById("Fri").style.display="block";
-    }else{
-        document.getElementById("others").style.display="block";
-    }
-     
-  }
-
-
-  function adjustRating(rating) {
-    document.getElementById("ratingvalue").innerHTML = rating;
-}
-
-function selectResponse() {
-	const s = document.querySelector('#selected')
-	const sel = document.querySelector('#selectbrowser');
-	s.style.display = "block";
-	s.textContent = sel.value;
-	
-}
+  .then(function (jsonObject) {
+     console.table(jsonObject);  // temporary checking for valid response and data parsing
+     const towns = jsonObject['towns'];
+         for (let i = 0; i < towns.length; i++ ) {
+             let card = document.createElement('section');
+             let h1 = document.createElement('h1');
+             let motto = document.createElement('p');
+             let population = document.createElement('p');
+             let rain = document.createElement('p');
+             let image = document.createElement('img');
+             
+             h1.textContent = towns[i].name;
+             motto.textContent = towns[i].motto;
+             population.textContent = "Population : " + towns[i].currentPopulation;
+             rain.textContent = "Annual Rain Fall : " + towns[i].averageRainfall;
+             image.setAttribute('src', towns[i].photo);
+             
+ 
+             card.appendChild(h1);
+             card.appendChild(motto);
+             card.appendChild(population);
+             card.appendChild(rain);
+             card.appendChild(image);
+             card.setAttribute("class", "card");
+             
+             document.querySelector("div.cards").appendChild(card);
+         }
+             
+   });
