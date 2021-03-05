0cad0bef8f485de45b76b5fa2de2e8b865986a77
Use your GitHub username <Enter your email adderss>
2021-03-06 02:40:45 +0900

    initial commit

--- lesson9/js/script.js
+++ lesson9/js/script.js
@@ -9,6 +9,7 @@ fetch(requestURL)
      console.table(jsonObject);  // temporary checking for valid response and data parsing
      const towns = jsonObject['towns'];
          for (let i = 0; i < towns.length; i++ ) {
+            if(i==5){
              let card = document.createElement('section');
              let h1 = document.createElement('h1');
              let motto = document.createElement('p');
@@ -31,6 +32,53 @@ fetch(requestURL)
              card.setAttribute("class", "card");
              
              document.querySelector("div.cards").appendChild(card);
+            } else  if(i==1){
+                let card = document.createElement('section');
+                let h1 = document.createElement('h1');
+                let motto = document.createElement('p');
+                let population = document.createElement('p');
+                let rain = document.createElement('p');
+                let image = document.createElement('img');
+                
+                h1.textContent = towns[i].name;
+                motto.textContent = towns[i].motto;
+                population.textContent = "Population : " + towns[i].currentPopulation;
+                rain.textContent = "Annual Rain Fall : " + towns[i].averageRainfall;
+                image.setAttribute('src', towns[i].photo);
+                
+    
+                card.appendChild(h1);
+                card.appendChild(motto);
+                card.appendChild(population);
+                card.appendChild(rain);
+                card.appendChild(image);
+                card.setAttribute("class", "card");
+                
+                document.querySelector("div.cards").appendChild(card);
+               } else if (i==6){
+                let card = document.createElement('section');
+                let h1 = document.createElement('h1');
+                let motto = document.createElement('p');
+                let population = document.createElement('p');
+                let rain = document.createElement('p');
+                let image = document.createElement('img');
+                
+                h1.textContent = towns[i].name;
+                motto.textContent = towns[i].motto;
+                population.textContent = "Population : " + towns[i].currentPopulation;
+                rain.textContent = "Annual Rain Fall : " + towns[i].averageRainfall;
+                image.setAttribute('src', towns[i].photo);
+                
+    
+                card.appendChild(h1);
+                card.appendChild(motto);
+                card.appendChild(population);
+                card.appendChild(rain);
+                card.appendChild(image);
+                card.setAttribute("class", "card");
+                
+                document.querySelector("div.cards").appendChild(card);
+               } 
          }
              
    });
