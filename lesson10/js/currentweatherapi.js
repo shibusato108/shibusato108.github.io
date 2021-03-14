4487a822f3725bce292428c3848a2d8e72802ee0
Use your GitHub username <Enter your email adderss>
2021-03-14 10:38:00 +0900

    initial commit

--- lesson10/js/currentweatherapi.js
+++ lesson10/js/currentweatherapi.js
@@ -31,11 +31,7 @@ fetch(apiURL)
     document.getElementById(`wind`).textContent = chillFactor.toFixed(2); 
 
 
-    //const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-    //const desc = jsObject.weather[0].description;
-    //document.getElementById('imagesrc').textContent = imagesrc;
-    //document.getElementById('icon').setAttribute('src', imagesrc);
-    //document.getElementById('icon').setAttribute('alt', desc);
+    
    
    
   });
